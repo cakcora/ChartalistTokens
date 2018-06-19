88c01fb0b396132bf98106a1f83632bdaffb0362
anonym <anonyma@gmail.com>
2018-06-18 20:37:28 -0500

    edges added

--- src/main/java/params/Params.java
+++ src/main/java/params/Params.java
@@ -10,8 +10,8 @@ public class Params {
     public static String dir = "D:/Ethereum/fromVadim/";
     public static boolean countPerDay = true;
     public static boolean storeContractTransactions = true;
-    public static String tokenInfoFile = "D:/Dropbox/Publications/PostDoc work/Chartalist/data/top200Tokens.csv";
-    public static String erc20FunctionsFile = "D:/Dropbox/Publications/PostDoc work/Chartalist/data/erc20FunctionsFile.txt";
+    public static String tokenInfoFile = "D:/Dropbox/Publications/PostDoc work/3 - Chartalist/data/top200Tokens.csv";
+    public static String erc20FunctionsFile = "D:/Dropbox/Publications/PostDoc work/3 - Chartalist/data/erc20FunctionsFile.txt";
     public static int ercStandardFunctionLength = 10;
     public static int eth64 = 64;
     public static String tokenFilesDir ="D:/Ethereum/tokens/";
